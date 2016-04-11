b014be264f7a444bcd06833329fbe8743d0ed373
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-04-11 21:19:07 +0200

    simplify test code

--- spec/runner/tool_spec.rb
+++ spec/runner/tool_spec.rb
@@ -9,23 +9,25 @@ describe RunnerTool::Backends::Ssh do
     double("ssh_connection")
   end
 
+  let(:block) do
+    Proc.new do |cmd|
+      exec! "foobar"
+    end
+  end
+
   before(:each) do
     allow(ssh_connection).to receive(:exec!)
     allow(Net::SSH).to receive(:start).and_yield(ssh_connection)
   end
 
   it "should call the exec method witht he right parameters" do
-    expect(subject).to receive(:exec!).with("foo")
-    subject.exec(host) do |cmd|
-      exec! "foo"
-    end
+    expect(subject).to receive(:exec!).with("foobar")
+    subject.exec(host,  &block)
   end
 
   it "should set the right hostname" do
-    expect(RunnerTool::Command).to receive(:new).with(host, "bar").and_call_original
-    subject.exec(host) do |cmd|
-      exec! "bar"
-    end
+    expect(RunnerTool::Command).to receive(:new).with(host, "foobar").and_call_original
+    subject.exec(host,  &block)
   end
 
 end
